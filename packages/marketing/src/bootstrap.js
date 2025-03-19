0d7dc331d8bb1e3dc1379b0376f883fa0fd49a37
Jeremiah Spradlin <jspradlin@otldap.net>
2025-03-19 11:01:27 -0600

    Added history - Browser and Memory to App

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
-    ReactDOM.render(<App />, el);  
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history} />, el);  
+
+    return {
+        onParentNavigate({ pathname: nextPathName }) {
+            const { pathname } = history.location
+
+            if (pathname !== nextPathName) {
+                history.push(nextPathName);
+            }
+
+        }
+    }
 };
 
 
 // If we are in DEV and Isolation
 //      Call mount immediately
 if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
+    const devRoot = document.querySelector('#_marketing-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory(0) });
     }
 }
 
